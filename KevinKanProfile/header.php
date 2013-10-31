f96d0bfdc41e3f0a2dcfdae33efdbaa669d14823
EncoderZero <intrepidhonor@gmail.com>
2013-10-31 14:04:26 -0400

    add mobile detection and some icons for the navigation

--- KevinKanProfile/header.php
+++ KevinKanProfile/header.php
@@ -1,3 +1,7 @@
+<?php 
+session_start();
+$_SESSION['forceDesktop']=false;
+?>
 <!DOCTYPE html>
 <html>
 <!-- Written By: Kevin Kan
@@ -13,8 +17,22 @@
 		//get the current page name
 		$currentPage=preg_replace($fileTypes,'', basename($_SERVER['SCRIPT_NAME'])); 
 		//vars to hold all css and script files to be linked depending on script name with default common files on all pages
-		$cssFiles=array("css/site.css");
-		$scriptFiles=array('http://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js');
+		$cssFiles=array("css/site.css","//netdna.bootstrapcdn.com/font-awesome/4.0.1/css/font-awesome.css");
+		$scriptFiles=array('http://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js','http://code.jquery.com/ui/1.10.3/jquery-ui.js');
+		//check if mobile
+		$userString=strtoupper($_SERVER['HTTP_USER_AGENT']);
+		$listOfMobileUserAgents=array("/IPHONE/","/ANDROID/","/IPOD/","/BLACKBERRY/","/BLAZER/","/BOLT/", "/MOBILE/");
+		$isMobile=false;
+		foreach($listOfMobileUserAgents as $pattern){
+			echo $pattern."\n";
+			if (preg_match($pattern,$userString)==true){
+				$isMobile=true;
+			}
+		}
+		if($isMobile==true && $_SESSION['forceDesktop']==false){
+			array_push($cssFiles,"css/homePage.css");
+			array_push($scriptFiles,'http://code.jquery.com/mobile/1.3.2/jquery.mobile-1.3.2.min.js');
+		}
 		
 		if ($currentPage=='index'){//if home page
 			array_push($cssFiles,"css/homePage.css");
@@ -22,14 +40,13 @@
 			array_push($scriptFiles,'jquery/jquery.nivo.slider.pack.js');
 			array_push($scriptFiles,'jquery/featuredWorkSlider.js');
 		}
-		elseif ($currentPage!='services'){
+		elseif ($currentPage!='services'){//if services page
 			array_push($cssFiles,"css/".trim($currentPage).".css");
 		}
 		//add external css
 		foreach($cssFiles as $fileName){
-			echo"<link rel='stylesheet' type='text/css' href='".$fileName."' media='screen' />";
+			echo"<link rel='stylesheet' type='text/css' href='".$fileName."' media='screen' rel='stylesheet' />";
 		}
-		
 		//add external scripts
 		foreach($scriptFiles as $scriptName){
 			echo"<script src='".$scriptName."' type='text/javascript'></script>";
@@ -42,12 +59,12 @@
 		<nav> 
 			<img id='logoImg' src="media/logo.png" alt='Kevin Kan Logo'/>
 			<ul>
-				<a href='index.php'><li <?php echo ($currentPage=='index') ? "class='activeNav'":NULL;     ?>>Home</li></a>
-				<a href='aboutMe.php'><li <?php echo ($currentPage=='aboutMe') ? "class='activeNav'":NULL;   ?>>About Me</li></a>
-				<a href='projects.php'><li <?php echo ($currentPage=='projects') ? "class='activeNav'":NULL;  ?>>Projects</li></a>
-				<a href='services.php'><li <?php echo ($currentPage=='services') ? "class='activeNav'":NULL;  ?>>Services</li></a>
-				<a href='https://github.com/EncoderZero/'><li>GitHub</li></a>
-				<a href='contactMe.php'><li <?php echo ($currentPage=='contactMe') ? "class='activeNav'":NULL; ?>>Contact Me</li></a>
+				<a href='index.php'><li <?php echo ($currentPage=='index') ? "class='activeNav'":NULL;     ?>>Home</li> <i class="fa fa-home fa-fw"></i></a>
+				<a href='aboutMe.php'><li <?php echo ($currentPage=='aboutMe') ? "class='activeNav'":NULL;   ?>>About Me<i class="fa fa-smile-o fa-fw"></i></li></a>
+				<a href='projects.php'><li <?php echo ($currentPage=='projects') ? "class='activeNav'":NULL;  ?>>Projects <i class="fa fa-trophy fa-fw"></i></li></a>
+				<a href='services.php'><li <?php echo ($currentPage=='services') ? "class='activeNav'":NULL;  ?>>Services <i class="fa fa-suitcase fa-fw"></i></li></a>
+				<a href='https://github.com/EncoderZero/'><li>GitHub<i class="fa fa-github fa-fw"></i> </li></a>
+				<a href='contactMe.php'><li <?php echo ($currentPage=='contactMe') ? "class='activeNav'":NULL; ?>>Contact Me <i class="fa fa-phone fa-fw"></i></li></a>
 			</ul>
 			<div id='socialMediaLinks'>
 				<p>Stay Connected</p>
